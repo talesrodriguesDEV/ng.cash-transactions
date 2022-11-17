d415706a2fef4e397f8ae81ddab8519cd3fb02d2
Tales Rodrigues <tales00001@gmail.com>
2022-11-17 07:45:27 -0300

    Setting up CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 import UserRoutes from './routes/user.router';
 import LoginRoutes from './routes/login.router';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
+
 app.use('/users', UserRoutes);
 app.use('/login', LoginRoutes);
 
